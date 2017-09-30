bdb99d9275f19de01fe698d4363e3c6c79bd7cf4
YLiao <eray.liao@gmail.com>
2017-09-30 21:58:44 +1000

    profile activity finished

--- Driveroo/app/src/main/java/com/example/liaoy/driveroo/HomeActivity.java
+++ Driveroo/app/src/main/java/com/example/liaoy/driveroo/HomeActivity.java
@@ -1,9 +1,13 @@
 package com.example.liaoy.driveroo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,13 +15,28 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Date dt=new Date();
+        String hour=dt.toString().split(" ")[3].split(":")[0];
+        int hourInt=Integer.parseInt(hour);
+        TextView home_greeting= (TextView)findViewById(R.id.home_Greeting);
+        String username="Frank";
+
+        if (hourInt>=4 && hourInt<12) home_greeting.setText("Good morning, "+username+"!");
+        else {
+            if (hourInt >= 12 && hourInt < 18)
+                home_greeting.setText("Good afternoon, " + username + "!");
+            else {
+                home_greeting.setText("Good evening, " + username + "!");
+            }
+        }
+        
 
         ImageButton profiBtn= (ImageButton) findViewById(R.id.imageButton2);
         profiBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-              //  startActivity(profi_intent);
+                Intent profileIntent=new Intent(getApplicationContext(),ProfileActivity.class);
+                startActivity(profileIntent);
             }
         });
     }
